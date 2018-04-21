9f087580e8cf7fa499cde85798425b3263eda87e
amindler <amindler@gmail.com>
2018-04-21 05:25:16 -0700

    new yelow machinery texture for submachine gun so it's more like a power tool

---
+++ Assets/_MyContent/MyScripts/_UFPSExtended/FPWeaponItemUse.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPWeaponItemUse : vp_FPWeapon {
+
+	public override void Wield(bool isWielding = true)
+	{
+		base.Wield();
+		var c = this.gameObject.transform.GetChild(0);
+		vp_Layer.Set(c.gameObject, vp_Layer.Weapon, true);
+		if(c.GetChild(0) != null){
+			vp_Layer.Set(c.GetChild(0).gameObject, vp_Layer.Weapon, true);
+
+		}
+	}
+}
